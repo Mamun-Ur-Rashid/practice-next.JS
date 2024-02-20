2db75fa262b9191f9aa162bff06ff8ecf6459503
Mamun-Ur-Rashid <engmamun656@gmail.com>
2024-02-20 13:53:44 +0600

    learn loading page

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div>
+            <h2>LOading................</h2>
+        </div>
+    );
+};
+
+export default LoadingPage;
